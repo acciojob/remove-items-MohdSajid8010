b6b3c8c0c48c09868f6eec5eaff17e29a2da0ca8
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-04-27 17:40:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let color_arr=["Red","Green","White","Black"];
+let Sel_colorEl=	document.getElementById("colorSelect");
+
+document.getElementById("btn").addEventListener("click" , ()=>{
+let Sel_color=Sel_colorEl.value;
+	for(let i=0; i< color_arr.length; i++)
+		{
+			if(color_arr[i]==Sel_color)
+			{
+				color_arr.splice(i , 1);
+			}
+		}
+
+	Sel_colorEl.innerHTML="";
+	for(let i=0; i< color_arr.length; i++)
+		{
+			Sel_colorEl.innerHTML+=`<option>${color_arr[i]}</option> `;
+		}
+})
